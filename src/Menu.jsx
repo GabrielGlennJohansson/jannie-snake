7c4609bf90878dfe2f2cdf44b2385b6692ceb03e
Gabriel Johansson <g.g.johansson@outlook.com>
2026-04-25 14:16:04 +0200

    final fix img

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,14 +1,12 @@
-
-
 function Menu() {
   return (
-        <div className="menu">
-            <img src="public/img/banner.png" alt="Banner" />
-            <ul>
-                <li><a href="#">Highscore</a></li>
-            </ul>
-        </div>
-    )
+    <div className="menu">
+      <img src={`${import.meta.env.BASE_URL}img/banner.png`} alt="Banner" />
+      <ul>
+        <li><a href="#">Highscore</a></li>
+      </ul>
+    </div>
+  )
 }
 
 export default Menu
